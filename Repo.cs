901da7409fe8f767e8beda7923b9e6e0d93334c0
Mike Schwartz <Michael.Schwartz@spacex.com>
2019-10-27 23:06:14 -0700

    Fix formatting and add missing comma

--- Repo.cs
+++ Repo.cs
@@ -10,45 +10,45 @@ namespace MachOneSoftware.PizzaBuddy
             new Dictionary<string, string[]>()
             {
                 { "Barbeque Chicken",   new[] { "onion", "chicken", "bbq sauce"}},
-                { "Beef Lovers",        new[] { "pepperoni", "beef" } },
-                { "Breakfast",          new[] { "bacon", "boiled egg" } },
-                { "Buffalo Chicken",    new[] { "chicken", "buffalo sauce" } },
-                { "Cheesesteak",        new[] { "beef", "green pepper", "onion", "mushroom" } },
-                { "Chicken Bacon Alfredo",      new[] { "mushroom", "onion", "chicken", "bacon"}},
-                { "Chicken Feast",      new[] { "chicken", "mushroom", "sweetcorn" } },
-                { "Deluxe Hawaiian",    new[] { "ham", "bacon", "pineapple" } },
-                { "Diavolo"             new[] { "pepperoni", "salami", "onion" } },
-                { "Four Seasons",       new[] {"tomato", "mozzarella cheese", "basil leaves", "artichoke",
-                                               "fontina cheese", "mushroom", "red pepper", "provolone cheese", "potato",
-                                               "pecorino cheese"}},
-                { "Hawaiian",           new[] { "ham", "pineapple" } },
-                { "Hawaiian Elvis",     new[] { "canadian bacon", "pineapple", "sauerkraut" } },
-                { "Meat Lovers",        new[] { "pepperoni", "beef", "sausage", "ham" } },
-                { "New York Classic",   new[] { "double cheese", "pepperoni", "hot chorizo sausage"} },
-                { "Pepperoni Bacon",    new[] { "pepperoni", "bacon" } },
-                { "Pepperoni Passion",  new[] { "pepperoni" } },
-                { "Pepperoni Veggie",   new[] { "pepperoni", "onion", "green pepper" } },
-                { "Philly Cheesesteak",     new[] { "mozzarella cheese", "seasoned steak", "bell peppers"} },
-                { "Prosciutto and Arugula",   new[] { "mozzarella chesse", "parmesan", "prosciutto", "arugula" } },
-                { "Ranch BBQ",          new[] { "chicken", "pepperoni", "beef", "bacon"} },
-		            { "Seafood",     	      new[] { "shrimp", "anchovy" , "basil" } },
-                { "Supreme",            new[] { "pepperoni", "sausage", "mushroom", "green pepper", "onion" } },
-                { "Texas BBQ",          new[] { "chicken", "bacon", "onion", "red pepper", "green pepper" } },
-                { "Tuna Supreme",       new[] { "tuna", "sweetcorn", "onion" } }
+                { "Beef Lovers",        new[] { "pepperoni", "beef" }},
+                { "Breakfast",          new[] { "bacon", "boiled egg" }},
+                { "Buffalo Chicken",    new[] { "chicken", "buffalo sauce" }},
+                { "Cheesesteak",        new[] { "beef", "green pepper", "onion", "mushroom" }},
+                { "Chicken Bacon Alfredo",  new[] { "mushroom", "onion", "chicken", "bacon"}},
+                { "Chicken Feast",      new[] { "chicken", "mushroom", "sweetcorn" }},
+                { "Deluxe Hawaiian",    new[] { "ham", "bacon", "pineapple" }},
+                { "Diavolo",            new[] { "pepperoni", "salami", "onion" }},
+                { "Four Seasons",       new[] { "tomato", "mozzarella cheese", "basil leaves", "artichoke",
+                                                "fontina cheese", "mushroom", "red pepper", "provolone cheese", "potato",
+                                                "pecorino cheese" }},
+                { "Hawaiian",           new[] { "ham", "pineapple" }},
+                { "Hawaiian Elvis",     new[] { "canadian bacon", "pineapple", "sauerkraut" }},
+                { "Meat Lovers",        new[] { "pepperoni", "beef", "sausage", "ham" }},
+                { "New York Classic",   new[] { "double cheese", "pepperoni", "hot chorizo sausage" }},
+                { "Pepperoni Bacon",    new[] { "pepperoni", "bacon" }},
+                { "Pepperoni Passion",  new[] { "pepperoni" }},
+                { "Pepperoni Veggie",   new[] { "pepperoni", "onion", "green pepper" }},
+                { "Philly Cheesesteak",     new[] { "mozzarella cheese", "seasoned steak", "bell peppers" }},
+                { "Prosciutto and Arugula", new[] { "mozzarella chesse", "parmesan", "prosciutto", "arugula" }},
+                { "Ranch BBQ",          new[] { "chicken", "pepperoni", "beef", "bacon"}},
+		        { "Seafood",            new[] { "shrimp", "anchovy" , "basil" }},
+                { "Supreme",            new[] { "pepperoni", "sausage", "mushroom", "green pepper", "onion" }},
+                { "Texas BBQ",          new[] { "chicken", "bacon", "onion", "red pepper", "green pepper" }},
+                { "Tuna Supreme",       new[] { "tuna", "sweetcorn", "onion" }}
             }
         );
 
         private static Lazy<Dictionary<string, string[]>> _veggiePizzas = new Lazy<Dictionary<string, string[]>>(() =>
             new Dictionary<string, string[]>()
             {
-                { "Balkan",             new[] {"goat cheese", "cherry tomato", "black olive" }},
-                { "Margherita",         new[] {"basil", "tomato", "garlic" } },
-                { "Mushroom Broccoli",  new[] {"mushroom", "broccoli" } },
-                { "Neopolitan",         new[] { "basil leaves", "tomato", "mozarella cheese"} },
-                { "Parmigiana",         new[] {"tomato", "eggplant" } },
-                { "Spice Garden",       new[] {"green pepper", "banana pepper", "jalapeño pepper" } },
-                { "Spinach Delight",    new[] {"spinach", "tomato" } },
-                { "Veggie Lovers",      new[] {"tomato", "olive", "spinach", "mushroom", "onion" } }
+                { "Balkan",             new[] { "goat cheese", "cherry tomato", "black olive" }},
+                { "Margherita",         new[] { "basil", "tomato", "garlic" }},
+                { "Mushroom Broccoli",  new[] { "mushroom", "broccoli" }},
+                { "Neopolitan",         new[] { "basil leaves", "tomato", "mozarella cheese" }},
+                { "Parmigiana",         new[] { "tomato", "eggplant" }},
+                { "Spice Garden",       new[] { "green pepper", "banana pepper", "jalapeño pepper" }},
+                { "Spinach Delight",    new[] { "spinach", "tomato" }},
+                { "Veggie Lovers",      new[] { "tomato", "olive", "spinach", "mushroom", "onion" }}
             }
         );
 
